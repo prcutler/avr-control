6119b028b48158812b9dc510d638aa017e26d7d9
Paul Cutler <paul@paulcutler.org>
2020-12-21 15:43:45 -0600

    Basic functionality is working
    You can click Volume Up or Down and the volume adjusts.
    Need to figure out a way to keep the menu bar persistent
    when changing

--- avr.py
+++ avr.py
@@ -1,9 +1,28 @@
+import denonavr
 import rumps
+
+zone2 = {"Zone2": "Office"}
+d = denonavr.DenonAVR("192.168.1.212", add_zones=zone2)
+d.update()
 
 
 class AvrApp(object):
     def __init__(self):
         self.app = rumps.App("AVR", "🎛️")
+
+    print(d.volume, d.zones["Zone2"].volume)
+
+    @rumps.clicked("Volume +")
+    def volume_up(self):
+        d.zones["Zone2"].volume_up()
+        d.zones["Zone2"].update()
+        print("Up volume is: ", d.zones["Zone2"].volume)
+
+    @rumps.clicked("Volume -")
+    def volume_up(self):
+        d.zones["Zone2"].volume_down()
+        d.zones["Zone2"].update()
+        print("Down volume is: ", d.zones["Zone2"].volume)
 
     def run(self):
         self.app.run()
